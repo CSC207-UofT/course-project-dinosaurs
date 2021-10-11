933ea3a5896e0b3f7f4307e58be6ec0f5db03338
ericyin <ericyin23@gmail.com>
2021-10-10 21:34:38 -0400

    Added very basic code for TaskManager and Task. Added two very simple test cases for TaskManager to make sure priority sorting is working (see TODOs for more information).

--- src/main/java/DueDateComparator.java
+++ src/main/java/DueDateComparator.java
@@ -4,11 +4,12 @@ class DueDateComparator implements Comparator<Task> {
 
     /**
      * A comparator which compares two due dates between tasks.
-     * @param t1
-     * @param t2
-     * @return
+     * @param t1 The first task's due date to be compared.
+     * @param t2 The second task's due date to be compared.
+     * @return A negative integer if t1's due date is before
+     * t2's due date. 0 if the due dates are equal. A positive
+     * integer if t1's due date is after t2's.
      */
-
 
     @Override
     public int compare(Task t1, Task t2){
