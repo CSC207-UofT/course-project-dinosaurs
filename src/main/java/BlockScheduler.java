6ed46dcb46e446d65155f7da83a3e34d23117f87
jebthom <jebthomas@gmail.com>
2021-10-12 17:34:32 -0400

    Basic code framework for BlockScheduler, needs getters and setters from other classes Implemented pomodoro for StudyMethod Nothing done yet in StudyBlock

---
+++ src/main/java/BlockScheduler.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.time.ZonedDateTime;
+
+public class BlockScheduler {
+    /**
+     * Creates a new StudyBlock based on the selected blockLength,
+     * studyMethod, chosenTasks, and priorityType.
+     */
+    private double maxLength;
+    private ArrayList<Integer> studyMethod;
+    private ArrayList chosenTasks;
+    private String priorityType;
+    // TODO figure out type of chosenTasks: list of Tasks
+
+    /**
+     * Constructor for the BlockScheduler.
+     *
+     * @param blockLength Time of desired StudyBlock in minutes.
+     * @param studyMethod Preferred study scheduling method.
+     * @param chosenTasks List of Tasks to be completed.
+     * @param priorityType How the Tasks will be prioritized.
+     */
+    public BlockScheduler(double blockLength, ArrayList<Integer> studyMethod,
+                          ArrayList chosenTasks, String priorityType) {
+        this.maxLength = blockLength;
+        this.studyMethod = studyMethod;
+        this.chosenTasks = chosenTasks;
+        this.priorityType = priorityType;
+    }
+
+    /**
+     * Schedules a new StudyBlock.
+     * TODO figure out how to represent, with breaks splitting task chunks appropriately
+     * @return Prioritized StudyBlock of specified blockLength.
+     */
+    public StudyBlock schedule() {
+        StudyBlock newBlock = new StudyBlock();
+        List<Task> taskList = new ArrayList<>(this.chosenTasks); // shallow copy should be okay?
+        // TODO TaskManager sorts taskList by priorityType
+
+        while (newBlock.length() < this.maxLength) {
+            // TODO implement all below
+            for (Task task : chosenTasks) {
+                newBlock.add(task);
+                newBlock.addLength(task.getLength());
+                // add break time to total time
+                newBlock.addLength(this.studyMethod.get(1));
+            }
+        }
+        // fill in extra space
+        if (newBlock.getLength() != this.maxLength) {
+            // TODO if there is extra time what do we do here? add a task or notify?
+        }
+        return newBlock;
+    }
+}
---
+++ src/main/java/StudyMethod.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class StudyMethod {
+    /**
+     * All of the supported StudyMethods used by BlockScheduler.
+     * Tells BlockScheduler how to divide study and break time.
+     *
+     * preferred_method.size() == 2
+     * preferred_method[0] is active time in minutes
+     * preferred_method[1] is break time in minutes
+     *
+     * TODO implement additional methods
+     * TODO move Pomodoro to constants
+     */
+    private static final ArrayList<Integer> POMODORO = new ArrayList<>(Arrays.asList(25, 5));
+    private ArrayList<Integer> preferred_method;
+
+    /**
+     * Constructor for the StudyMethod.
+     * Currently, preferred_method.size() == 2 only
+     * @param method The preferred study scheduling method
+     *               must be in accepted methods
+     */
+    public StudyMethod(ArrayList<Integer> method) {
+        this.preferred_method = method;
+    }
+
+    /**
+     * Returns this instance's preferred study method.
+     * @return preferred_method This instance's chosen study method
+     */
+    public ArrayList<Integer> getMethod() {
+        return this.preferred_method;
+    }
+
+    /**
+     * Sets a new preferred study method.
+     * @param method The preferred study scheduling method
+     * @return true if the method was set successfully.
+     */
+    public boolean setMethod(ArrayList<Integer> method) {
+        if (method.equals(POMODORO)) {
+            this.preferred_method = POMODORO;
+            return true;
+        }
+        // TODO add more methods
+        return false;
+    }
+}
