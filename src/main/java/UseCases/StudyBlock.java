fa46f55acfa39d25fe3429700d09c98713d1f73f
ericyin <ericyin23@gmail.com>
2021-10-25 16:28:39 -0400

    Did half of the function for build todo list

--- src/main/java/UseCases/StudyBlock.java
+++ src/main/java/UseCases/StudyBlock.java
@@ -4,6 +4,7 @@ import Entities.Checklist;
 import Entities.StudyMethod;
 import Entities.Task;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class StudyBlock {
@@ -50,6 +51,35 @@ public class StudyBlock {
             x -= this.studyMethod.getMethod().get(0);
         }
         this.listTODO = msg;
+    }
+
+    /**
+     * Breaks up the study block into a two dimensional Array which stores the
+     * different increments of active and break time.
+     * @param length The length of the study block in minutes.
+     */
+    private int[][] breakUpStudyBlock(int length){
+        // We first need the number of full blocks we'll have and the amount of extra time leftover.
+        // Note that extra + blocks == length
+        int blocks = length / this.studyMethod.getMethod().get(0) + this.studyMethod.getMethod().get(1);
+        int extra = length % this.studyMethod.getMethod().get(0) + this.studyMethod.getMethod().get(1);
+        // We return a 2D array where the length is the number of block-breaks and the indexes of each
+        // array are the amount of time for the block and break.
+        int[][] array = new int[blocks + 1][2];
+        for (int i = 0; i < blocks; i++) {
+            array[i] = new int[]{this.studyMethod.getMethod().get(0), this.studyMethod.getMethod().get(1)};
+        }
+        // If the extra time is less than the active study block, tag it on as an extra active study block
+        // If the extra time is more than the active study block, tag it on as a full
+        // active study block and an extra break.
+        // Note that if there is no extra time, this last index will be [0][0].
+        if (extra < this.studyMethod.getMethod().get(0)){
+            array[blocks] = new int[]{extra, 0};
+        } else {
+            array[blocks] = new int[]{this.studyMethod.getMethod().get(0), extra - this.studyMethod.getMethod().get(0)};
+        }
+        return array;
+
     }
 
     public String toString() {
