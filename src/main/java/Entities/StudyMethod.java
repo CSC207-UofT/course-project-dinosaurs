d453d540b3ef97bf8462ff6968d173a26113248d
Paridhi <paridhi.desai@mail.utoronto.ca>
2021-11-13 14:33:41 -0500

    Few changes that I made to SM

--- src/main/java/Entities/StudyMethod.java
+++ src/main/java/Entities/StudyMethod.java
@@ -11,8 +11,8 @@ public class StudyMethod {
      * preferred_method[0] is active time in minutes
      * preferred_method[1] is break time in minutes
      *
-     * TODO implement additional methods
-     * TODO move Pomodoro to constants
+     *
+     *
      */
     public static final ArrayList<Integer> POMODORO = new ArrayList<>(Arrays.asList(25, 5));
     public static final ArrayList<Integer> DESKTIME = new ArrayList<>(Arrays.asList(52, 17));
@@ -27,6 +27,15 @@ public class StudyMethod {
      */
     public StudyMethod(ArrayList<Integer> method) {
         this.preferred_method = method;
+        if (preferred_method == POMODORO){
+            System.out.print("Let's study using POMODORO");
+        }
+        if (preferred_method == DESKTIME) {
+            System.out.print("Let's study using DESKTIME");
+        }
+        if (preferred_method == ULTRADIUM) {
+            System.out.print("Let's study using ULTRADIUM");
+        }
     }
 
     /**
@@ -43,12 +52,15 @@ public class StudyMethod {
      * @param method The preferred study scheduling method
      * @return true if the method was set successfully.
      */
+
+    /**
     public boolean setMethod(ArrayList<Integer> method) {
-        if (method.equals(POMODORO)) {
-            this.preferred_method = POMODORO;
-            return true;
-        }
-        if (method.equals(DESKTIME)) {
+        this.preferred_method = method;
+     }
+    */
+
+        /**
+         if (method.equals(DESKTIME)) {
             this.preferred_method = DESKTIME;
             return true;
         }
@@ -59,4 +71,5 @@ public class StudyMethod {
         // TODO add more methods
         return false;
     }
+    */
 }
