93c5e7257d52fb0ff4ed5ca0f307e61368c6b797
zoyaharoun <zoya.haroun@mail.utoronto.ca>
2021-11-21 18:52:57 -0500

    studyblock methods

--- src/main/java/Controllers/addStudyBlockController.java
+++ src/main/java/Controllers/addStudyBlockController.java
@@ -13,26 +13,26 @@ import static Constants.Constants.DUE_DATE;
 import static Controllers.Data.checklistList;
 import static Controllers.Data.studyBlockList;
 
-public class addStudyBlockController {
-    public void addStudyBlock(ActionEvent actionEvent, String name, int length) {
-        /* static protected List<StudyBlock> studyBlockList = new ArrayList<>();
-
-
-
-            this.studyMethod = studyMethod;
-            this.checklist = checklist;
-            this.listTODO = new ArrayList<>();
-            this.name = name;
-            this.length = length;
-
-         */
-        //todo : fix later(add preference,checklist, length )
-        Checklist list = TempCreator.createTemp("examplename", checklistList, DUE_DATE);
-        StudyMethod method = new StudyMethod(StudyMethod.POMODORO);
-
-        StudyBlock block = new StudyBlock(name, method, list, length);
-        studyBlockList.add(block);
-
-    }
-
-}
+//public class addStudyBlockController {
+//    public void addStudyBlock(ActionEvent actionEvent, String name, int length) {
+//        /* static protected List<StudyBlock> studyBlockList = new ArrayList<>();
+//
+//
+//
+//            this.studyMethod = studyMethod;
+//            this.checklist = checklist;
+//            this.listTODO = new ArrayList<>();
+//            this.name = name;
+//            this.length = length;
+//
+//         */
+//        //todo : fix later(add preference,checklist, length )
+//        Checklist list = TempCreator.createTemp("examplename", checklistList, DUE_DATE);
+//        StudyMethod method = new StudyMethod(StudyMethod.POMODORO);
+//
+//        StudyBlock block = new StudyBlock(name, method, list, length);
+//        studyBlockList.add(block);
+//
+//    }
+//
+//}
