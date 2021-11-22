e33143fbf9b934c8dabe480d7d4478529166374c
Paridhi <paridhi.desai@mail.utoronto.ca>
2021-11-22 15:32:35 -0500

    addStudyBlockController

--- src/main/java/Entities/StudyMethod.java
+++ src/main/java/Entities/StudyMethod.java
@@ -7,13 +7,10 @@ public class StudyMethod implements Serializable {
     /**
      * All of the supported StudyMethods used by BlockScheduler.
      * Tells BlockScheduler how to divide study and break time.
-     *
+     * <p>
      * preferred_method.size() == 2
      * preferred_method[0] is active time in minutes
      * preferred_method[1] is break time in minutes
-     *
-     *
-     *
      */
     public static final ArrayList<Integer> POMODORO = new ArrayList<>(Arrays.asList(25, 5));
     public static final ArrayList<Integer> DESKTIME = new ArrayList<>(Arrays.asList(52, 17));
@@ -23,12 +20,13 @@ public class StudyMethod implements Serializable {
     /**
      * Constructor for the Entities.StudyMethod.
      * Currently, preferred_method.size() == 2 only
+     *
      * @param method The preferred study scheduling method
      *               must be in accepted methods
      */
     public StudyMethod(ArrayList<Integer> method) {
         this.preferred_method = method;
-        if (preferred_method == POMODORO){
+        if (preferred_method == POMODORO) {
             System.out.print("Let's study using POMODORO");
         }
         if (preferred_method == DESKTIME) {
@@ -41,6 +39,7 @@ public class StudyMethod implements Serializable {
 
     /**
      * Returns this instance's preferred study method.
+     *
      * @return preferred_method This instance's chosen study method
      */
     public ArrayList<Integer> getMethod() {
@@ -50,18 +49,16 @@ public class StudyMethod implements Serializable {
 
     /**
      * Sets a new preferred study method.
+     *
      * @param method The preferred study scheduling method
      * @return true if the method was set successfully.
      */
 
-    /**
-    public boolean setMethod(ArrayList<Integer> method) {
-        this.preferred_method = method;
-     }
-    */
 
-        /**
-         if (method.equals(DESKTIME)) {
+    public boolean setMethod(ArrayList<Integer> method) {
+
+        this.preferred_method = method;
+        if (method.equals(DESKTIME)) {
             this.preferred_method = DESKTIME;
             return true;
         }
@@ -69,8 +66,11 @@ public class StudyMethod implements Serializable {
             this.preferred_method = ULTRADIUM;
             return true;
         }
-        // TODO add more methods
+        if (method.equals(POMODORO)) {
+            this.preferred_method = POMODORO;
+            return true;
+            }
         return false;
     }
-    */
 }
+
